7681426e34d03c1644f0877c435d9180395c5379
Khangarid <khangarid.davaadorj@gmail.com>
2018-06-10 17:41:30 -0500

    desiging

--- src/modules/board/containers/Board.js
+++ src/modules/board/containers/Board.js
@@ -16,6 +16,56 @@ const data = [
     name: 'Чи надад дурлаасай',
     artist: 'Болд',
     ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'Odoo',
+    artist: 'Lil Thug E',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'WITH YOU',
+    artist: 'TSETSE ft. NMN',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: '1 2 3',
+    artist: 'NAMONE ft. BIL G',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'Өвөлжөө (Mongolian version)',
+    artist: 'Magnolian',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'Hamtdaa',
+    artist: 'TATAR & Maraljingoo',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'Чамд үлдээе',
+    artist: 'Choi Joo',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'СҮҮН ДАЛАЙ ЭЭЖ',
+    artist: 'Б.Аззаяа',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'WITH YOU',
+    artist: 'TSETSE ft. NMN',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: '1 2 3',
+    artist: 'NAMONE ft. BIL G',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
+  },
+  {
+    name: 'Өвөлжөө (Mongolian version)',
+    artist: 'Magnolian',
+    ytUrl: 'https://www.youtube.com/watch?v=OLCpV2NZW9Y'
   }
 ];
 
