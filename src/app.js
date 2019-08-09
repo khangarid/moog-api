e5cbe730ae80e2509c56bb11a9e9dc87ba3b6ca0
Khangarid <khangarid.davaadorj@gmail.com>
2019-08-08 21:55:10 -0400

    refactor: restructure project

---
+++ src/app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const environment = require('../config/environment')
+
+require('./models/User');
+require('./models/Tag');
+require('./models/Post');
+
+require('./services/passport');
+
+mongoose.connect(environment.mongoURI);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [environment.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT);
